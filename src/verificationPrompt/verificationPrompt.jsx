f222f64787fca2fbabedd6fb3e85dd90b12e8f1e
bryan-agustin-pajarillaga <bryannn747.com>
2024-12-20 20:43:01 +0800

    s

--- src/verificationPrompt/verificationPrompt.jsx
+++ src/verificationPrompt/verificationPrompt.jsx
@@ -1,13 +1,16 @@
 import "./verificationPrompt.css"
 export default function VerificationPrompt({Hint, showVerificationPrompt, setShowVerificationPrompt}){
     return(
-        <div className={ showVerificationPrompt ? "VerificationPrompt" : "unShowVerificationPrompt"}>
-            <h1>Would You Take A Hint?</h1>
-
-            <div className="display_Flex">
-                <button className="options" onClick={()=>setShowVerificationPrompt(false)}>No</button>
-                <button className="options" onClick={()=>{Hint("true")}}>Yes</button>
-            </div>
-        </div>  
+        <>
+            <div className={showVerificationPrompt ? "WrapVerificationPrompt" : "unShowWrapVerificationPrompt"}></div>
+            <div className={ showVerificationPrompt ? "VerificationPrompt" : "unShowVerificationPrompt"}>
+               <h1>Would You Take A Hint?</h1>
+   
+               <div className="display_Flex">
+                   <button className="options" onClick={()=>setShowVerificationPrompt(false)}>No</button>
+                   <button className="options" onClick={()=>{Hint("true")}}>Yes</button>
+               </div>
+           </div>  
+        </>
     )
 }
